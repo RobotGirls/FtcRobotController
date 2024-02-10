88bf3ae2b55a7d1d50aa9ad0bb25c1aa3f32bb55
katelynl19 <katelynliu@rocknrollrobots25.com>
2024-02-10 14:20:49 -0800

    made MeepMeep paths for auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepCSBlueRight.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepCSBlueRight.java
@@ -1,47 +1,55 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepCSBlueLeft {
+public class MeepMeepCSBlueRight {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Required: Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(12, 58, Math.toRadians(270)))
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, 58, Math.toRadians(270)))
+
                                 //LEFT SPIKE
-                                /*
                                 .forward(27)
                                 .turn(Math.toRadians(90))
                                 // * deploy purple pixel
                                 .forward(-3)
-                                .strafeLeft(25)
-                                .lineToLinearHeading(new Pose2d(50, 36, Math.toRadians(180)))
+                                //.strafeRight(30)
+                                .lineToLinearHeading(new Pose2d(-39, 10, Math.toRadians(180)))
+                                .forward(-87)
+                                .strafeRight(30)
                                 // * deploy yellow pixel
 
-                                 */
+
+
                                 // CENTER SPIKE
-
-                                .forward(26)
+/*
+                                .forward(24)
                                 // * deploy purple pixel
-                                .forward(-25)
-                                .lineToLinearHeading(new Pose2d(50, 36, Math.toRadians(180)))
+                                .forward(-5)
+                                .strafeRight(20)
+                                .forward(28)
+                                .turn(Math.toRadians(-90))
+                                .forward(-103)
+                                .strafeRight(22)
                                 // * deploy yellow pixel
-
-
+ */
 
                                 // RIGHT SPIKE
                                 /*
                                 .forward(26)
                                 .turn(Math.toRadians(-90))
                                 .forward(-3)
-                                .lineToLinearHeading(new Pose2d(50, 36, Math.toRadians(180)))
-                                */
+                                .lineToLinearHeading(new Pose2d(-34, 10, Math.toRadians(180)))
+                                .forward(-85)
+                                .strafeRight(22)
+
+                                 */
 
                                 .build()
 
